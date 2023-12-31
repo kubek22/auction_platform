c3ca36e120b41f9652c60f4bcd43ff6f46fb5bd6
kubek22 <01171264@pw.edu.pl>
2023-12-31 17:38:09 +0100

    creating auction models

--- auction/models.py
+++ auction/models.py
@@ -1,5 +1,22 @@
 from django.db import models
 
+from user.models import User
+
+
 # Create your models here.
 
 
+class Item(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=256, blank=True, default='')
+    photo = models.ImageField()
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Auction(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    entry_price = models.DecimalField(decimal_places=2, max_digits=10)
+    #currency
+    active = models.BooleanField()
